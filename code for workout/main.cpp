28368d52beefc9bb3e457ab448915db1c728a46f
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-06-21 19:20:02 +0530

    Added kill as TASKKILL Formatted Code

--- code for workout/main.cpp
+++ code for workout/main.cpp
@@ -2,20 +2,22 @@
 #include <bits/stdc++.h>
 #include <windows.h>
 #include <conio.h>
-#define second 7
 using namespace std;
 
+#define second 7
+#define kill "TASKKILL /f /t /IM microsoft.photos.exe 1>NULL"
+
 int main();
-#include "yoga_part.cpp"
-#include "exercise_part.cpp"
+#include "./assets/Functions/yoga_part.cpp"
+#include "./assets/Functions/exercise_part.cpp"
 
 bool pushups_gif = true;
-int ch_exercise = 1,sec;
+int ch_exercise = 1, sec;
 
 int starting_timer()
 {
     system("CLS");
-    cout << "\tLets begin the cardio session in\n"
+    cout << "\tLet's begin the cardio session in\n"
          << endl;
     cout << setfill(' ') << setw(28) << "---------" << endl;
     cout << setfill(' ') << setw(20) << "| " << sec << "  sec  |" << endl;
@@ -31,48 +33,55 @@ void pushup()
         system((path + "\\Pushups\\pushups.gif").c_str());
         pushups_gif = false;
     }
+
     fstream fpushups((path + "\\Pushups\\Description.txt").c_str(), ios::in);
     getline(fpushups, st, '\0');
     cout << st;
     fpushups.close();
 }
 
-
 int main()
 {
-	while(true){
-    cout << "\t----------------------------------------";
-    cout << "\n\t******* Welcome To Workout Part *******" << endl;
-    cout << "\t----------------------------------------";
-    cout << "\n\t1.Cardio\n\t2.Yoga\n\tEsc to exit " << endl;
-    cout << "\t";
-    char ch;
-    ch=_getch();
-    system("CLS");
-    switch (ch)
+    while (true)
     {
-    case 49:
-		 sec = 5;
-        while (sec >= 0)
+        cout << "\t----------------------------------------";
+        cout << "\n\t******* Welcome To Workout Part *******" << endl;
+        cout << "\t----------------------------------------";
+        cout << "\n\t1.Cardio\n\t2.Yoga\n\tEsc to exit " << endl;
+        cout << "\t";
+
+        char ch;
+        ch = _getch();
+        
+        system("CLS");
+        switch (ch)
         {
-            starting_timer();
-            //            Sleep(1000);
-            sec--;
+        case 49:
+            sec = 5;
+            while (sec >= 0)
+            {
+                starting_timer();
+                // Sleep(1000);
+                sec--;
+            }
+            system("CLS");
+            exercise_function();
+            break;
+
+        case 50:
+            yoga_function();
+            break;
+
+        case 27:
+            exit(0);
+
+        default:
+            cout << "\nInvalid Input\n\n";
+            Sleep(1000);
+            system("CLS");
+            break;
         }
-        system("CLS");
-        exercise_function();
-        break;
-    case 50:
-        yoga_function();
-        break;
-   case 27:
-   		exit(0);
-    default:
-        cout << "\nInvalid Input\n\n";
-        Sleep(1000);
-        system("CLS");
-        break;
     }
-}
+
     return 0;
 }
